9c92aa2a44117d78b3eb9f36c8f5dd0dcaf9f5aa
Vincent Fu <vincent.fu@apn.com.au>
2017-09-13 17:24:05 +1000

    counting inversions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Algorithms
+## Algorithms (Node.js)
 
 1. Karatsuba Multiplication Algorithm
 2. Counting Inversions (Divide and Conquer)
--- inversions/inversions.js
+++ inversions/inversions.js
@@ -2,31 +2,64 @@
 
 'use strict';
 
- const fs = require('fs');
- const readline = require('readline');
+const fs = require('fs');
+const readline = require('readline');
 
- const arr = [];
- const rl = readline.createInterface({
-	 input: fs.createReadStream('test.txt')
- });
+const arr = [];
+const rl = readline.createInterface({
+	input: fs.createReadStream('integers.txt')
+});
 
- rl.on('line', (line) => {
-	 arr.push(line);
- });
+rl.on('line', (line) => {
+	arr.push(line);
+});
 
- rl.on('close', () => {
-	 countInversions(arr);
- });
+rl.on('close', () => {
+	let inv = new Inversion();
+	inv.countInv(arr);
+	console.log(inv.count)
+});
 
-function countInversions(arr) {
-	let n = arr.length;
-	if(n <= 1) return 0;
+class Inversion {
+	constructor() {
+		this.count = 0;
+	}
+	 
+	countInv(arr) {
+		let len = arr.length;
+		if(len === 1) return arr;
 
-	let mid = n / 2;
-	let l = arr.slice(0, mid);
-	let r = arr.slice(mid);
-}
+		let mid = Math.floor(len / 2);
+		let l = arr.slice(0, mid);
+		let r = arr.slice(mid);
 
-function merge() {
+		let sortedL = this.countInv(l);
+		let sortedR = this.countInv(r);
 
+		return this.merge(sortedL, sortedR);
+	}
+
+	merge(l, r) {
+		let sortedArr = [];
+		let lLen = l.length;
+		let rLen = r.length;
+		let i = 0;
+		let j = 0;
+
+		while(i < lLen && j < rLen) {
+			if(Number(l[i]) < Number(r[j])) {
+				sortedArr.push(l[i]);
+				i++;
+			} else {
+				sortedArr.push(r[j]);
+				// Black magic is here
+				this.count += l.slice(i).length;
+				j++;
+			}
+		}
+
+		let restL = l.slice(i);
+		let restR = r.slice(j);
+		return sortedArr.concat(restL).concat(restR);
+	}
 }
