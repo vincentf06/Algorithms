041186c8cbfef1858e7d921cff96452508553b6b
Vincent Fu <vincent.fu@apn.com.au>
2017-09-19 18:05:33 +1000

    updated quick sort

--- quick-sort/quick-sort.js
+++ quick-sort/quick-sort.js
@@ -1,4 +1,5 @@
 /* Counting comparisons in quick sort */
+/* Use the "median-of-three" pivot rule */
 
 'use strict';
 
@@ -17,6 +18,7 @@ rl.on('line', (line) => {
 rl.on('close', () => {
 	let sorter = new QuickSort();
 	sorter.sort(arr, 0, 9999);
+	console.log(arr)
 	console.log(sorter.comparisons);
 });
 
@@ -29,11 +31,27 @@ class QuickSort {
 		let len = rInd - lInd + 1;
 		if(len < 2 ) return;
 
-		let pivotInd = lInd;
-		let parInd = this._partition(arr, lInd, rInd, pivotInd);
+		let pivotInd = this._choosePivot(arr, lInd, rInd);
+		// exchange pivot element with first element of sub array
+		[arr[pivotInd], arr[lInd]] = [arr[lInd], arr[pivotInd]];
+		let parInd = this._partition(arr, lInd, rInd, lInd);
 		
 		this.sort(arr, lInd, parInd - 1);
 		this.sort(arr, parInd + 1, rInd);
+	}
+
+	_choosePivot(arr, lInd, rInd) {
+		// Choose the first one if there are only two entries
+		if(rInd - lInd + 1 === 2) return lInd;
+
+		let midInd = Math.floor((rInd - lInd) / 2);
+		let indices = Array.of(lInd, midInd, rInd);
+		let values = indices.map(ind => arr[ind]);
+		for(let ind of indices) {
+			if(arr[ind] > Math.min(...values) && arr[ind] < Math.max(...values)) {
+				return ind;
+			}
+		}
 	}
 
 	_partition(arr, lInd, rInd, pivotInd) {
