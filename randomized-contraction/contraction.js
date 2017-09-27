648a28da68dd4238b78d360667deb3b1d1e04af9
Vincent Fu <vincentf06@gmail.com>
2017-09-27 21:43:04 +1000

    updated karger's min cut

--- randomized-contraction/contraction.js
+++ randomized-contraction/contraction.js
@@ -6,17 +6,45 @@
 const fs = require('fs');
 const readline = require('readline');
 
+
 const adjacencyList = [];
+const edges = [];
 const rl = readline.createInterface({
 	input: fs.createReadStream('graph.txt'),
 });
 
 rl.on('line', (line) => {
-	var list = line.split('\t');
-	list.shift();
+	const list = line.split('\t');
+	// The last entry is an empty string, remove it.
+	list.pop();
+	const v = list.shift();
 	adjacencyList.push(list);
+
+	for(let u of list) {
+		const edge = [v, u];
+		edges.push(edge);
+	}
+
 });
 
 rl.on('close', () => {
-	console.log(adjacencyList);
-})
+	const minCut = getMinCut(adjacencyList, edges);
+	// console.log(minCut);
+});
+
+function getMinCut(adjacencyList, edges) {
+	if(adjacencyList.length === 2) return;
+
+	const edge = getRandomEdge(edges);
+	console.log(edge);
+	removeEdge(edges, edge);
+}
+
+function getRandomEdge(edges) {
+	const index = Math.floor(Math.random() * edges.length);
+	return edges[index];
+}
+
+function removeEdge(edges, edge) {
+	
+}
